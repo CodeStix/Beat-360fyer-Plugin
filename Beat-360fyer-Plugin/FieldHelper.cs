4a71494b0b10c9e80c45cc990fd33e030cee7ce6
Stijn <reddusted@gmail.com>
2021-02-03 14:50:16 +0100

    Support custom levels

--- Beat-360fyer-Plugin/FieldHelper.cs
+++ Beat-360fyer-Plugin/FieldHelper.cs
@@ -14,9 +14,25 @@ namespace Beat_360fyer_Plugin
             return (T)obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic).GetValue(obj);
         }
 
-        public static void Set(object obj, string fieldName, object value)
+        public static bool TryGet<T>(object obj, string fieldName, out T val)
         {
-            obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic).SetValue(obj, value);
+            FieldInfo f = obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (f == null)
+            {
+                val = default;
+                return false;
+            }
+            val = (T)f.GetValue(obj);
+            return true;
+        }
+
+        public static bool Set(object obj, string fieldName, object value)
+        {
+            FieldInfo f = obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (f == null)
+                return false;
+            f.SetValue(obj, value);
+            return true;
         }
     }
 }
--- Beat-360fyer-Plugin/Patches/LevelUpdatePatcher.cs
+++ Beat-360fyer-Plugin/Patches/LevelUpdatePatcher.cs
@@ -15,11 +15,66 @@ namespace Beat_360fyer_Plugin.Patches
     {
         static void Prefix(Action beforeSceneSwitchCallback, bool practice, ref LevelSelectionNavigationController ___levelSelectionNavigationController)
         {
-            Plugin.Log.Info("starting level: " + ___levelSelectionNavigationController.selectedDifficultyBeatmap.SerializedName());
+            IDifficultyBeatmap bm = ___levelSelectionNavigationController.selectedDifficultyBeatmap;
+
+            Plugin.Log.Info("starting level: " + bm.SerializedName());
+
+            if (bm.parentDifficultyBeatmapSet.beatmapCharacteristic.serializedName == GameModeHelper.GENERATED_360DEGREE_MODE)
+            {
+                Plugin.Log.Info("starting generated 360");
+                bm.beatmapData.AddBeatmapEventData(new BeatmapEventData(1f, BeatmapEventType.Event15, 1));
+                bm.beatmapData.AddBeatmapEventData(new BeatmapEventData(2f, BeatmapEventType.Event15, 2));
+                bm.beatmapData.AddBeatmapEventData(new BeatmapEventData(3f, BeatmapEventType.Event15, 3));
+                bm.beatmapData.AddBeatmapEventData(new BeatmapEventData(4f, BeatmapEventType.Event15, 4));
+            }
         }
     }
 
-    [HarmonyPatch(typeof(LevelCollectionViewController))]
+    [HarmonyPatch(typeof(StandardLevelDetailView))]
+    [HarmonyPatch("SetContent", MethodType.Normal)]
+    public class LevelUpdatePatcher
+    {
+        static void Prefix(StandardLevelDetailView __instance, IBeatmapLevel level, BeatmapDifficulty defaultDifficulty, BeatmapCharacteristicSO defaultBeatmapCharacteristic, PlayerData playerData, bool showPlayerStats)
+        {
+            Plugin.Log.Info("[SetContent] song " + level.songName);
+            Plugin.Log.Info("[SetContent] type " + level.GetType().FullName);
+
+            if (level.beatmapLevelData.difficultyBeatmapSets.Any((e) => e.beatmapCharacteristic.serializedName == GameModeHelper.GENERATED_360DEGREE_MODE))
+            {
+                Plugin.Log.Info("[SetContent] already registered new gamemode");
+                return;
+            }
+
+            IDifficultyBeatmapSet standard = level.beatmapLevelData.difficultyBeatmapSets.FirstOrDefault((e) => e.beatmapCharacteristic.serializedName == "Standard");
+            if (standard == null)
+            {
+                Plugin.Log.Info("[SetContent] standard is null");
+                return;
+            }
+
+            BeatmapCharacteristicSO customGameMode = GameModeHelper.GetGenerated360GameMode();
+            CustomDifficultyBeatmapSet custom360DegreeSet = new CustomDifficultyBeatmapSet(customGameMode);
+            CustomDifficultyBeatmap[] difficulties = standard.difficultyBeatmaps.Select((e) => new CustomDifficultyBeatmap(e.level, custom360DegreeSet, e.difficulty, e.difficultyRank, e.noteJumpMovementSpeed, e.noteJumpStartBeatOffset, e.beatmapData)).ToArray();
+            custom360DegreeSet.SetCustomDifficultyBeatmaps(difficulties);
+
+            IDifficultyBeatmapSet[] newSets = level.beatmapLevelData.difficultyBeatmapSets.AddItem(custom360DegreeSet).ToArray();
+            
+            if (level.beatmapLevelData is BeatmapLevelData data)
+            {
+                if (!FieldHelper.Set(data, "_difficultyBeatmapSets", newSets))
+                {
+                    Plugin.Log.Info("[SetContent] could not set new difficulty sets");
+                    return;
+                }
+            }
+            else
+            {
+                Plugin.Log.Info("[SetContent] unsupported data: " + (level.beatmapLevelData?.GetType().FullName ?? "null"));
+            }
+        }
+    }
+
+    /*[HarmonyPatch(typeof(LevelCollectionViewController))]
     [HarmonyPatch("HandleLevelCollectionTableViewDidSelectLevel", MethodType.Normal)]
     class LevelSelectPatcher
     {
@@ -27,61 +82,29 @@ namespace Beat_360fyer_Plugin.Patches
 
         static void Prefix(LevelCollectionTableView tableView, IPreviewBeatmapLevel level) 
         {
-            Plugin.Log.Info("HandleLevelCollectionTableViewDidSelectLevel");
-            if(level == null)
-            {
-                Plugin.Log.Info("level is null");
-                return;
-            }
-
-            IBeatmapLevel level3 = level as IBeatmapLevel;
-            if(level3 == null)
-            {
-                Plugin.Log.Info("IPreviewBeatmapLevel is not IBeatmapLevel? " + level.GetType().FullName);
-                return;
-            }
-
-            if (level3.beatmapLevelData.difficultyBeatmapSets.Any((e) => e.beatmapCharacteristic.serializedName == GENERATED_GAME_MODE))
-            {
-                Plugin.Log.Info("already registered new gamemode");
-                return;
-            }
-
-            IDifficultyBeatmapSet[] difficultySets = level3.beatmapLevelData.difficultyBeatmapSets;// Fields.Get<IDifficultyBeatmapSet[]>(level3.beatmapLevelData, "_difficultyBeatmapSets");
-            if (difficultySets == null)
-            {
-                Plugin.Log.Info("difficultySets is null");
-                return;
-            }
-
-            BeatmapCharacteristicSO customGameMode = GameModeHelper.GetGenerated360GameMode();
-
-            IDifficultyBeatmapSet standard = level3.beatmapLevelData.difficultyBeatmapSets.FirstOrDefault((e) => e.beatmapCharacteristic.serializedName == "Standard");
-            if (standard == null)
-            {
-                Plugin.Log.Info("standard is null");
-                return;
-            }
-
-            CustomDifficultyBeatmapSet custom360DegreeSet = new CustomDifficultyBeatmapSet(customGameMode);
-
-            CustomDifficultyBeatmap[] difficulties = standard.difficultyBeatmaps.Select((e) => new CustomDifficultyBeatmap(e.level, custom360DegreeSet, e.difficulty, e.difficultyRank, e.noteJumpMovementSpeed, e.noteJumpStartBeatOffset, e.beatmapData)).ToArray();
-            custom360DegreeSet.SetCustomDifficultyBeatmaps(difficulties);
-
-            difficultySets = difficultySets.AddToArray(custom360DegreeSet);
-            FieldHelper.Set(level3.beatmapLevelData, "_difficultyBeatmapSets", difficultySets);
-
-            Plugin.Log.Info("created generated 360 gamemode");
+            
         }
-    }
+    }*/
 
-    [HarmonyPatch(typeof(StandardLevelDetailView))]
+    /*[HarmonyPatch(typeof(StandardLevelDetailView))]
     [HarmonyPatch("RefreshContent", MethodType.Normal)]
     public class LevelUpdatePatcher
     {
         static void Prefix(StandardLevelDetailView __instance, ref IBeatmapLevel ____level, ref IDifficultyBeatmap ____selectedDifficultyBeatmap)
         {
-            __instance.actionButtonText = ____level.songName;
+            Plugin.Log.Info("[RefreshContent] refresh " + ____level.previewDifficultyBeatmapSets.Length + " , " + ____level.beatmapLevelData.difficultyBeatmapSets.Length);
+            Plugin.Log.Info("[RefreshContent] type2 " + ____level.GetType().FullName);
+
+            //if (!FieldHelper.Set(____level.beatmapLevelData, "_difficultyBeatmapSets", ____level.beatmapLevelData.difficultyBeatmapSets.AddItem()))
+            //{
+            //    Plugin.Log.Info("cleared");
+            //}
+
+            Plugin.Log.Info("[RefreshContent] song " + (____level?.songName ?? "null"));
+
+            //__instance.actionButtonText = ____level.songName;
         }
-    }
+    }*/
 }
+
+
