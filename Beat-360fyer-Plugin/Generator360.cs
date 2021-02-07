c350afa9ce6173e9578d918796285dc06a9ca4a8
Stijn <reddusted@gmail.com>
2021-02-07 17:11:22 +0100

    Seperate generator class + fixed wall cutting and spinning

---
+++ Beat-360fyer-Plugin/Generator360.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beat_360fyer_Plugin
+{
+    public class Generator360
+    {
+        // The amount of rotations before stopping rotation events (rip cable otherwise)
+        public int LimitRotations { get; set; }= 28;
+        // Enable the spin effect when no notes are coming
+        public bool EnableSpin { get; set; } = true;
+        public float TotalSpinTime { get; set; } = 0.4f;
+        // Amount of time in seconds to cut of the front/back of a wall when rotating towards it
+        public float WallFrontCut { get; set; } = 0.065f;
+        public float WallBackCut { get; set; } = 0.125f;
+        // Maximum amount of rotation events per second
+        public int MaxRotationsPerSecond { get; set; } = 8;
+
+        public void Generate(IDifficultyBeatmap bm)
+        {
+            // Amount of rotation events emitted
+            int eventCount = 0;
+            // Current rotation
+            int rotation = 0;
+            // Moments where a wall should be cut
+            List<(float, int)> wallCutMoments = new List<(float, int)>();
+            // Previous spin direction, false is left, true is right
+            bool previousDirection = false;
+            // The time of the previous rotation event that was emitted
+            float previousRotationTime = 0f;
+            // Time of previous note
+            float spinTimer = float.MaxValue;
+
+            // Negative numbers rotate to the left, positive to the right
+            void Rotate(float time, int amount)
+            {
+                if (amount == 0)
+                    return;
+                if (amount < -4)
+                    amount = -4;
+                if (amount > 4)
+                    amount = 4;
+                if (rotation + amount > LimitRotations || rotation + amount < -LimitRotations)
+                    return;
+                previousDirection = amount > 0;
+                previousRotationTime = time;
+                time += 0.002f; // Small delay to place the rotation event after the note
+                rotation += amount;
+                eventCount++;
+                wallCutMoments.Add((time, amount));
+                bm.beatmapData.AddBeatmapEventData(new BeatmapEventData(time, BeatmapEventType.Event15, amount > 0 ? 3 + amount : 4 + amount));
+            }
+
+            // The time of 1 beat in seconds
+            float beatDuration = 60f / bm.level.beatsPerMinute;
+
+            Plugin.Log.Info($"[Generator] Start, bpm={bm.level.beatsPerMinute} beatDuration={beatDuration}");
+
+            List<BeatmapObjectData> objects = new List<BeatmapObjectData>(bm.beatmapData.beatmapObjectsData);
+            List<ObstacleData> obstacles = new List<ObstacleData>();
+            List<NoteData> currentNotes = new List<NoteData>();
+            for (int i = 0; i < objects.Count; i++)
+            {
+                currentNotes.Clear();
+
+                // All the currentNotes will have around the same time (negligible, just use the first note's time)
+                float time = objects[i].time;
+
+                for (; i < objects.Count && (objects[i].time - time) * beatDuration <= 0.01f; i++)
+                {
+                    BeatmapObjectData d = objects[i];
+                    if (d is NoteData n)
+                    {
+                        if (n.cutDirection != NoteCutDirection.None) // Filter bombs
+                            currentNotes.Add(n);
+                    }
+                    else if (d is ObstacleData o)
+                    {
+                        obstacles.Add(o);
+                    }
+                }
+
+                // If only bombs/obstacles
+                if (currentNotes.Count == 0)
+                {
+                    spinTimer = time;
+                    continue;
+                }
+
+                // Bottleneck rotation events
+                if ((time - previousRotationTime) * beatDuration < 1f / MaxRotationsPerSecond)
+                {
+                    //Plugin.Log.Info($"{currentNotes.Count} notes bottlenecked at {time} ({time} - {previousRotationTime}) * {beatDuration} = {(time - previousRotationTime) * beatDuration} (< 0.125f)");
+                    spinTimer = time;
+                    continue;
+                }
+
+                // Spin effect
+                if (EnableSpin && (time - spinTimer) * beatDuration >= TotalSpinTime * 1.5f)
+                {
+                    Plugin.Log.Info($"[Generator] Spin effect at {spinTimer}: ({time} - {spinTimer}) * {beatDuration} = {(time - spinTimer) * beatDuration}");
+                    float spinStep = TotalSpinTime / 24 / beatDuration;
+                    int spinDirection = previousDirection ? -1 : 1;
+                    for (int s = 0; s < 24; s++)
+                    {
+                        Rotate(spinTimer + spinStep * s, spinDirection);
+                    }
+                }
+
+                // Amount of total notes, notes pointing to the left/right
+                int count = currentNotes.Count;
+                int leftCount = currentNotes.Count((e) =>
+                        e.cutDirection == NoteCutDirection.DownLeft
+                    || e.cutDirection == NoteCutDirection.Left
+                    || e.cutDirection == NoteCutDirection.UpLeft
+                    || ((e.lineIndex == 0 || e.lineIndex == 1) && (e.cutDirection != NoteCutDirection.Right && e.cutDirection != NoteCutDirection.DownRight && e.cutDirection != NoteCutDirection.UpRight) && e.noteLineLayer == NoteLineLayer.Base)); // (e.cutDirection == NoteCutDirection.Left && e.noteLineLayer != NoteLineLayer.Top) ||
+                int rightCount = currentNotes.Count((e) =>
+                    e.cutDirection == NoteCutDirection.DownRight
+                    || e.cutDirection == NoteCutDirection.Right
+                    || e.cutDirection == NoteCutDirection.UpRight
+                    || ((e.lineIndex == 2 || e.lineIndex == 3) && (e.cutDirection != NoteCutDirection.Left && e.cutDirection != NoteCutDirection.DownLeft && e.cutDirection != NoteCutDirection.UpLeft) && e.noteLineLayer == NoteLineLayer.Base));
+
+                //Plugin.Log.Info($"{count} notes (<-{leftCount} {rightCount}->) at {time} [+{time - previousRotationTime} beats / +{(time - previousRotationTime) * beatDuration} seconds]");
+
+                int dir = -leftCount + rightCount;
+                if (dir < 0)
+                {
+                    Rotate(time, dir / 4 - 1);
+                }
+                else if (dir > 0)
+                {
+                    Rotate(time, dir / 4 + 1);
+                }
+                else // dir == 0
+                {
+                    Rotate(time, previousDirection ? -1 : 1);
+                }
+
+                spinTimer = time;
+            }
+
+            // Cut walls, walls will be cut when a rotation event is emitted
+            foreach (ObstacleData ob in obstacles)
+            {
+                foreach ((float cutTime, int cutAmount) in wallCutMoments)
+                {
+                    // If wall is uncomfortable for 360Degree mode, remove it
+                    if (ob.lineIndex == 1 || ob.lineIndex == 2 || (ob.obstacleType == ObstacleType.FullHeight && ob.lineIndex == 0 && ob.width > 1))
+                    {
+                        // TODO: Wall is not fun in 360, remove it
+                        FieldHelper.SetProperty(ob, nameof(ob.duration), 0f);
+                    }
+                    // If moved in direction of wall
+                    else if ((ob.lineIndex <= 1 && cutAmount < 0) || (ob.lineIndex >= 2 && cutAmount > 0))
+                    {
+                        float wallStartCutBeats = WallFrontCut / beatDuration;
+                        if (cutTime >= ob.time - wallStartCutBeats && cutTime < ob.time + ob.duration / 2f)
+                        {
+                            // Cut front of wall
+                            float cut = cutTime - (ob.time - wallStartCutBeats);
+
+                            Plugin.Log.Info($"[Generator] Cut front wall at {ob.time}({ob.duration}) cut {cut}");
+
+                            //ob.time += cut;
+                            //FieldHelper.SetProperty(ob, nameof(ob.time), ob.time + cut);
+                            ob.MoveTime(ob.time + cut);
+                            //ob.duration -= cut;
+                            FieldHelper.SetProperty(ob, nameof(ob.duration), ob.duration - cut);
+                        }
+
+                        float wallEndCutBeats = WallBackCut / beatDuration;
+                        if (cutTime >= ob.time + ob.duration / 2 && cutTime < ob.time + ob.duration + wallEndCutBeats)
+                        {
+                            // Cut back of wall
+                            float cut = (ob.time + ob.duration + wallEndCutBeats) - cutTime;
+
+                            Plugin.Log.Info($"[Generator] Cut back wall at {ob.time}({ob.duration}) cut {cut}");
+
+                            //ob.duration -= cut;
+                            FieldHelper.SetProperty(ob, nameof(ob.duration), ob.duration - cut);
+                        }
+                    }
+
+                    if (ob.duration <= 0f)
+                    {
+                        // TODO: remove wall instead of move to start
+                        ob.MoveTime(0f);
+                    }
+                }
+            }
+
+            //int removedWalls = objects.RemoveAll((e) => e is ObstacleData d && d.duration <= 0f);
+            //Plugin.Log.Info($"Removed {removedWalls} walls");
+
+            // Resort events...
+            // NOTE TO SELF: difficultyBeatmap.beatmapData.beatmapObjectsData/events MUST BE SORTED!
+            List<BeatmapEventData> sorted = bm.beatmapData.beatmapEventsData.OrderBy((e) => e.time).ToList();
+            FieldHelper.Set(bm.beatmapData, "_beatmapEventsData", sorted);
+
+            Plugin.Log.Info($"[Generator] Emitted {eventCount} rotation events");
+        }
+
+    }
+}
