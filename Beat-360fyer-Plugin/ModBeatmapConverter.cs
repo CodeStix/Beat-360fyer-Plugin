2dedabf1b3ce0db05c187f27f63e7329627a9f43
Stijn Rogiest <reddusted@gmail.com>
2021-07-06 16:21:00 +0200

    Revert "ModBeatmapConverter class"
    This reverts commit bdb4d3b18aa1e1fc391965a7c58701ef6f9ff2a3.

--- Beat-360fyer-Plugin/ModBeatmapConverter.cs
+++ Beat-360fyer-Plugin/ModBeatmapConverter.cs
@@ -1,11 +1,9 @@
-﻿using CustomJSONData;
-using CustomJSONData.CustomBeatmap;
+﻿using CustomJSONData.CustomBeatmap;
 using LibBeatGenerator;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +11,7 @@ namespace Beat360fyerPlugin
 {
     public static class ModBeatmapConverter
     {
-        private static IDictionary<string, object> CreateCustomDataDictionary(dynamic custom)
+        private static IDictionary<string, object> ConvertCustomData(dynamic custom)
         {
             if (custom is ExpandoObject && custom != null)
             {
@@ -25,69 +23,24 @@ namespace Beat360fyerPlugin
             }
         }
 
-        private static dynamic CreateDynamicCustomData(IDictionary<string, object> custom, IDifficultyBeatmap bm)
-        {
-            if (custom == null)
-            {
-                dynamic t = Trees.Tree();
-                // Workaround for NoodleExtensions error, why tf does this work??
-                t.bpm = bm.level.beatsPerMinute;
-                return bm;
-            }
-            else
-            {
-                return custom;
-            }
-        }
-
-        public static ModBeatmapData ToModBeatmap(this IDifficultyBeatmap bm)
+        public static void ToModBeatmap(this IDifficultyBeatmap bm)
         {
             ModBeatmapData mod = new ModBeatmapData(bm.beatmapData.numberOfLines, bm.level.beatsPerMinute);
             CustomBeatmapData data = (CustomBeatmapData)bm.beatmapData;
 
             foreach(CustomBeatmapEventData ev in data.beatmapEventsData)
             {
-                mod.events.Add(new ModBeatmapEventData(ev.time, (ModBeatmapEventType)ev.type, ev.value, CreateCustomDataDictionary(ev.customData)));
+                mod.events.Add(new ModBeatmapEventData(ev.time, (ModBeatmapEventType)ev.type, ev.value, ConvertCustomData(ev.customData)));
             }
 
             foreach (BeatmapObjectData obj in data.beatmapObjectsData)
             {
                 if (obj is CustomNoteData nd)
-                    mod.objects.Add(new ModNoteData(nd.time, nd.lineIndex, (ModNoteLineLayer)nd.noteLineLayer, (ModNoteCutDirection)nd.cutDirection, (ModColorType)nd.colorType, CreateCustomDataDictionary(nd.customData)));
+                    mod.objects.Add(new ModNoteData(nd.time, nd.lineIndex, (ModNoteLineLayer)nd.noteLineLayer, (ModNoteCutDirection)nd.cutDirection, (ModColorType)nd.colorType, ConvertCustomData(nd.customData)));
                 else if (obj is CustomObstacleData ob)
-                    mod.objects.Add(new ModObstacleData(ob.time, ob.lineIndex, (ModObstacleType)ob.obstacleType, ob.duration, ob.width, CreateCustomDataDictionary(ob.customData)));
+                    mod.objects.Add(new ModObstacleData(ob.time, ob.lineIndex, (ModObstacleType)ob.obstacleType, ob.duration, ob.width, ConvertCustomData(ob.customData)));
                 else if (obj is CustomWaypointData wd)
-                    mod.objects.Add(new ModWaypointData(wd.time, wd.lineIndex, (ModNoteLineLayer)wd.noteLineLayer, (ModOffsetDirection)wd.offsetDirection, CreateCustomDataDictionary(wd.customData)));
-            }
-
-            return mod;
-        }
-
-        public static void ToBeatmap(this ModBeatmapData from, IDifficultyBeatmap originalBeatmap)
-        {
-            CustomBeatmapData original = (CustomBeatmapData)originalBeatmap.beatmapData;
-
-            CustomBeatmapData bm = new CustomBeatmapData(from.NumberOfLines);
-            foreach(ModBeatmapEventData ev in from.events)
-            {
-                bm.AddBeatmapEventData(new CustomBeatmapEventData(ev.time, (BeatmapEventType)ev.type, ev.value, ev.customData));
-            }
-            foreach(ModBeatmapObjectData d in from.objects)
-            {
-                if (d is ModNoteData nd)
-                    bm.AddBeatmapObjectData((BeatmapObjectData)typeof(CustomNoteData).GetType().GetMethod("CreateBasicNoteData", BindingFlags.NonPublic).Invoke(null, new object[] { nd.time, nd.lineIndex, (NoteLineLayer)nd.noteLineLayer, (ColorType)nd.colorType, (NoteCutDirection)nd.cutDirection, CreateDynamicCustomData(nd.customData, originalBeatmap) }));
-                else if (d is ModObstacleData ob)
-                    bm.AddBeatmapObjectData(new CustomObstacleData(ob.time, ob.lineIndex, (ObstacleType)ob.obstacleType, ob.duration, ob.width, CreateDynamicCustomData(ob.customData, originalBeatmap)));
-                else if (d is ModWaypointData wd)
-                    bm.AddBeatmapObjectData(new CustomWaypointData(wd.time, wd.lineIndex, (NoteLineLayer)wd.noteLineLayer, (OffsetDirection)wd.offsetDirection, CreateDynamicCustomData(wd.customData, originalBeatmap)));
-            }
-
-            CustomBeatmapData.CopyAvailableSpecialEventsPerKeywordDictionary(original, bm);
-            CustomBeatmapData.CopyCustomData(original, bm);
-
-            if (!FieldHelper.Set(originalBeatmap, "_beatmapData", bm))
-            {
-                Plugin.Log.Error($"Could not replace beatmap");
+                    mod.objects.Add(new ModWaypointData(wd.time, wd.lineIndex, (ModNoteLineLayer)wd.noteLineLayer, (ModOffsetDirection)wd.offsetDirection, ConvertCustomData(wd.customData)));
             }
         }
         
