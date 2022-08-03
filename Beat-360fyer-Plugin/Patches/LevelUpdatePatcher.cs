d5774de57afd9878e2af897fdda5bf59cf6f8ac2
Stijn Rogiest <reddusted@gmail.com>
2022-08-04 00:00:21 +0200

    Use patch to override transformation phase of beatmaps, the plugin will act as another transformer

--- Beat-360fyer-Plugin/Patches/LevelUpdatePatcher.cs
+++ Beat-360fyer-Plugin/Patches/LevelUpdatePatcher.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using BeatmapSaveDataVersion3;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,56 +11,49 @@ using Zenject;
 
 namespace Beat360fyerPlugin.Patches
 {
-    [HarmonyPatch(typeof(MenuTransitionsHelper))]
-    [HarmonyPatch("StartStandardLevel", new[] { typeof(string), typeof(IDifficultyBeatmap), typeof(IPreviewBeatmapLevel), typeof(OverrideEnvironmentSettings), typeof(ColorScheme), typeof(GameplayModifiers), typeof(PlayerSpecificSettings), typeof(PracticeSettings), typeof(string), typeof(bool), typeof(Action), typeof(Action<DiContainer>), typeof(Action<StandardLevelScenesTransitionSetupDataSO, LevelCompletionResults>) })]
-    public class TransitionPatcher
+
+    [HarmonyPatch(typeof(BeatmapDataTransformHelper), "CreateTransformedBeatmapData")]
+    public class BeatmapDataTransformHelperPatcher
     {
-        private static HashSet<IDifficultyBeatmap> generated = new HashSet<IDifficultyBeatmap>();
-
-        static void Prefix(string gameMode, IDifficultyBeatmap difficultyBeatmap, OverrideEnvironmentSettings overrideEnvironmentSettings, ref ColorScheme overrideColorScheme, GameplayModifiers gameplayModifiers, PlayerSpecificSettings playerSpecificSettings, PracticeSettings practiceSettings, string backButtonText, bool useTestNoteCutSoundEffects, Action beforeSceneSwitchCallback, Action<DiContainer> afterSceneSwitchCallback, Action<StandardLevelScenesTransitionSetupDataSO, LevelCompletionResults> levelFinishedCallback)
+        static void Postfix(ref IReadonlyBeatmapData __result, IReadonlyBeatmapData beatmapData, IPreviewBeatmapLevel beatmapLevel, GameplayModifiers gameplayModifiers, bool leftHanded, EnvironmentEffectsFilterPreset environmentEffectsFilterPreset, EnvironmentIntensityReductionOptions environmentIntensityReductionOptions, MainSettingsModelSO mainSettingsModel)
         {
-            Plugin.Log.Info($"Starting ({difficultyBeatmap.GetType().FullName}) {difficultyBeatmap.SerializedName()} {gameMode} {difficultyBeatmap.difficulty} {difficultyBeatmap.level.songName}");
-
-            string startingGameModeName = difficultyBeatmap.parentDifficultyBeatmapSet.beatmapCharacteristic.serializedName;
-            if (startingGameModeName == GameModeHelper.GENERATED_360DEGREE_MODE || startingGameModeName == GameModeHelper.GENERATED_90DEGREE_MODE)
+            if (TransitionPatcher.startingGameMode == GameModeHelper.GENERATED_360DEGREE_MODE || TransitionPatcher.startingGameMode == GameModeHelper.GENERATED_90DEGREE_MODE)
             {
-                Plugin.Log.Info($"Generating rotation events for {startingGameModeName}...");
+                Plugin.Log.Info($"Generating rotation events for {TransitionPatcher.startingGameMode}...");
 
-                // Colors are not copied from standard mode for some reason? Enforce it here
-                var mapCustomColors = difficultyBeatmap.level.environmentInfo?.colorScheme?.colorScheme; 
-                if (mapCustomColors != null && overrideColorScheme == null)
+                Generator360 gen = new Generator360();
+                gen.WallGenerator = Config.Instance.EnableWallGenerator;
+                gen.OnlyOneSaber = Config.Instance.OnlyOneSaber;
+
+                if (TransitionPatcher.startingGameMode == GameModeHelper.GENERATED_90DEGREE_MODE)
                 {
-                    Plugin.Log.Info($"Overriding custom colors with {mapCustomColors.environmentColor0} {mapCustomColors.environmentColor1}");
-                    overrideColorScheme = mapCustomColors;
+                    gen.LimitRotations = Config.Instance.LimitRotations90;
+                    gen.BottleneckRotations = Config.Instance.LimitRotations90 / 2;
+                }
+                else if (TransitionPatcher.startingGameMode == GameModeHelper.GENERATED_360DEGREE_MODE)
+                {
+                    gen.LimitRotations = Config.Instance.LimitRotations360;
+                    gen.BottleneckRotations = Config.Instance.LimitRotations360 / 2;
                 }
 
-                if (!generated.Contains(difficultyBeatmap))
-                {
-                    generated.Add(difficultyBeatmap);
-                    Generator360 gen = new Generator360();
-                    gen.WallGenerator = Config.Instance.EnableWallGenerator;
-                    gen.OnlyOneSaber = Config.Instance.OnlyOneSaber;
-
-                    if (startingGameModeName == GameModeHelper.GENERATED_90DEGREE_MODE)
-                    {
-                        gen.LimitRotations = Config.Instance.LimitRotations90;
-                        gen.BottleneckRotations = Config.Instance.LimitRotations90 / 2;
-                    }
-                    else if (startingGameModeName == GameModeHelper.GENERATED_360DEGREE_MODE)
-                    {
-                        gen.LimitRotations = Config.Instance.LimitRotations360;
-                        gen.BottleneckRotations = Config.Instance.LimitRotations360 / 2;
-                    }
-
-                    gen.Generate(difficultyBeatmap);
-                }
-                else
-                {
-                    Plugin.Log.Info("Already generated rotation events");
-                }
+                __result = gen.Generate(__result, beatmapLevel.beatsPerMinute);
             }
         }
     }
+
+    [HarmonyPatch(typeof(MenuTransitionsHelper))]
+    [HarmonyPatch("StartStandardLevel", new[] { typeof(string), typeof(IDifficultyBeatmap), typeof(IPreviewBeatmapLevel), typeof(OverrideEnvironmentSettings), typeof(ColorScheme), typeof(GameplayModifiers), typeof(PlayerSpecificSettings), typeof(PracticeSettings), typeof(string), typeof(bool), typeof(bool), typeof(Action), typeof(Action<DiContainer>), typeof(Action<StandardLevelScenesTransitionSetupDataSO, LevelCompletionResults>) })]
+    public class TransitionPatcher
+    {
+        public static string startingGameMode;
+
+        static void Prefix(string gameMode, IDifficultyBeatmap difficultyBeatmap, IPreviewBeatmapLevel previewBeatmapLevel, OverrideEnvironmentSettings overrideEnvironmentSettings, ColorScheme overrideColorScheme, GameplayModifiers gameplayModifiers, PlayerSpecificSettings playerSpecificSettings, PracticeSettings practiceSettings, string backButtonText, bool useTestNoteCutSoundEffects, bool startPaused, Action beforeSceneSwitchCallback, Action<DiContainer> afterSceneSwitchCallback, Action<StandardLevelScenesTransitionSetupDataSO, LevelCompletionResults> levelFinishedCallback)
+        {
+            Plugin.Log.Info($"Starting ({difficultyBeatmap.GetType().FullName}) {difficultyBeatmap.SerializedName()} {gameMode} {difficultyBeatmap.difficulty} {difficultyBeatmap.level.songName}");
+            startingGameMode = difficultyBeatmap.parentDifficultyBeatmapSet.beatmapCharacteristic.serializedName;
+        }
+    }
+
 
     [HarmonyPatch(typeof(StandardLevelDetailView))]
     [HarmonyPatch("SetContent")]
@@ -91,10 +85,30 @@ namespace Beat360fyerPlugin.Patches
                     continue;
                 }
 
-                CustomDifficultyBeatmapSet customSet = new CustomDifficultyBeatmapSet(customGameMode);
-                CustomDifficultyBeatmap[] difficulties = basedOnGameMode.difficultyBeatmaps.Select((e) => new CustomDifficultyBeatmap(e.level, customSet, e.difficulty, e.difficultyRank, e.noteJumpMovementSpeed, e.noteJumpStartBeatOffset, e.beatmapData.GetCopy())).ToArray();
-                customSet.SetCustomDifficultyBeatmaps(difficulties);
-                sets.Add(customSet);
+                IDifficultyBeatmapSet newSet;
+                if (basedOnGameMode.difficultyBeatmaps[0] is BeatmapLevelSO.DifficultyBeatmap)
+                {
+                    IReadOnlyList<IDifficultyBeatmap> difficultyBeatmaps = basedOnGameMode.difficultyBeatmaps.Select((bm) => new BeatmapLevelSO.DifficultyBeatmap(bm.level, bm.difficulty, bm.difficultyRank, bm.noteJumpMovementSpeed, bm.noteJumpStartBeatOffset, FieldHelper.Get<BeatmapDataSO>(bm, "_beatmapData"))).ToList();
+                    newSet = new DifficultyBeatmapSet(customGameMode, difficultyBeatmaps);
+                }
+                else if (basedOnGameMode.difficultyBeatmaps[0] is CustomDifficultyBeatmap)
+                {
+                    CustomDifficultyBeatmapSet customSet = new CustomDifficultyBeatmapSet(customGameMode);
+                    CustomDifficultyBeatmap[] difficultyBeatmaps = basedOnGameMode.difficultyBeatmaps.Select((bm) =>
+                    {
+                        CustomDifficultyBeatmap cbm = (CustomDifficultyBeatmap)bm;
+                        return new CustomDifficultyBeatmap(cbm.level, customSet, cbm.difficulty, cbm.difficultyRank, cbm.noteJumpMovementSpeed, cbm.noteJumpStartBeatOffset, cbm.beatsPerMinute, cbm.beatmapSaveData, cbm.beatmapDataBasicInfo);
+                    }).ToArray();
+                    customSet.SetCustomDifficultyBeatmaps(difficultyBeatmaps);
+
+                    newSet = customSet;
+                }
+                else
+                {
+                    continue;
+                }
+
+                sets.Add(newSet);
             }
 
             // Update difficultyBeatmapSets
